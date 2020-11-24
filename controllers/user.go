bbe608cd6bbadb5795fb120af452bc9e2af5b509
Le H Ngo <seiji@o2.pl>
2020-11-24 13:55:34 +0000

    validator with translator to map for response

--- controllers/user.go
+++ controllers/user.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	en_translations "github.com/go-playground/validator/v10/translations/en"
 	"github.com/seijihg/api_template_mongodb/lib"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,11 +17,11 @@ import (
 
 // User struct definition. Uppercase for the JSON package to see their value.
 type User struct {
-	ID       primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
-	Email    string             `json:"email" bson:"email"`
+	ID       primitive.ObjectID `json:"_id" bson:"_id,omitempty" `
+	Email    string             `json:"email" bson:"email" validate:"required,email"`
 	Name     string             `json:"name" bson:"name"`
 	Surname  string             `json:"surname" bson:"surname"`
-	Password string             `json:"password" bson:"password"`
+	Password string             `json:"password" bson:"password" validate:"required"`
 	Dob      primitive.DateTime `json:"dob" bson:"dob"`
 }
 
@@ -34,7 +37,22 @@ func CreateUser(golangDB *mongo.Database) http.HandlerFunc {
 			return
 		}
 
-		usersCollection := golangDB.Collection("users")
+		// Validation User Struct at this point.
+		en := en.New()
+		uni := ut.New(en, en)
+		trans, _ := uni.GetTranslator("en")
+
+		validate := validator.New()
+		en_translations.RegisterDefaultTranslations(validate, trans)
+		err = validate.Struct(user)
+
+		if err != nil {
+			errs := err.(validator.ValidationErrors)
+			lib.WriteResponse(w, http.StatusBadRequest, errs.Translate(trans))
+			return
+		}
+
+		// usersCollection := golangDB.Collection("users")
 
 		// body, err := ioutil.ReadAll(r.Body)
 		// if err != nil {
@@ -57,15 +75,15 @@ func CreateUser(golangDB *mongo.Database) http.HandlerFunc {
 
 		user.Password = string(bcryptedPass)
 
-		res, err := usersCollection.InsertOne(context.TODO(), user)
-		if err != nil {
-			fmt.Println("InsertOne ERROR:", err)
-			lib.WriteResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
+		// res, err := usersCollection.InsertOne(context.TODO(), user)
+		// if err != nil {
+		// 	fmt.Println("InsertOne ERROR:", err)
+		// 	lib.WriteResponse(w, http.StatusBadRequest, err.Error())
+		// 	return
+		// }
 
-		fmt.Println("Response from DB:", res)
+		// fmt.Println("Response from DB:", res)
 
-		lib.WriteResponse(w, http.StatusOK, res)
+		// lib.WriteResponse(w, http.StatusOK, res)
 	}
 }
--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ require (
 	github.com/cosmtrek/air v1.21.2 // indirect
 	github.com/creack/pty v1.1.11 // indirect
 	github.com/fatih/color v1.10.0 // indirect
+	github.com/go-playground/locales v0.13.0
+	github.com/go-playground/universal-translator v0.17.0
+	github.com/go-playground/validator/v10 v10.4.1
 	github.com/gorilla/mux v1.8.0
 	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/pelletier/go-toml v1.8.1 // indirect
 	go.mongodb.org/mongo-driver v1.4.3
-	golang.org/x/crypto v0.0.0-20190530122614-20be4c3c3ed5
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/sys v0.0.0-20201117222635-ba5294a509c7 // indirect
 )
